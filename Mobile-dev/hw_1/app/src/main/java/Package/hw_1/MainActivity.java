bbca3840ef69d55a56d86c77b8be807ace7c535d
SUB (shokii) <shilo152004@mail.ru>
2024-02-13 20:09:07 +0300

    Add path for mobile-dev and starting hw_1

---
+++ Mobile-dev/hw_1/app/src/main/java/Package/hw_1/MainActivity.java
@@ -0,0 +1,19 @@
+package Package.hw_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Quit(View view) {
+        System.exit(0);
+    }
+}
