3696cd4831f06e9ebc207624646cf088cce5fc99
SUB (shokii) <shilo15200@mail.ru>
2024-02-24 09:31:24 +0300

    Done Python hw_5

--- Python/hw_5/main.py
+++ Python/hw_5/main.py
@@ -1,8 +1,13 @@
+import math
+
+
 def main(y, x):
     res = 0
     for i in range(len(y)):
-        res += (1 - 89 * x[i % 4] - y[len(y) + 1 - i] ** 3) ** 4
+        f_p = 1 - 89 * x[math.floor(i / 4)]
+        s_p = y[len(y) - 1 - i] ** 3
+        res += (f_p - s_p) ** 4
     return res
 
-# print("{:e}".format(main([-0.62, 0.04], [0.27, -0.54])))
-# HZ
+
+print("{:e}".format(main([-0.62, 0.04], [0.27, -0.54])))
