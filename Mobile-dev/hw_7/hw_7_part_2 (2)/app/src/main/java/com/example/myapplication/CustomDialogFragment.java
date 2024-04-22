71be4531f9317b4543e17ba736e57d8832ff1dba
SUB (shokii) <shilo15200@mail.ru>
2024-04-22 08:12:43 +0300

    Mobile-dev: Done hw_7 code

---
+++ Mobile-dev/hw_7/hw_7_part_2 (2)/app/src/main/java/com/example/myapplication/CustomDialogFragment.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class CustomDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+
+        return builder
+                .setTitle("Диалоговое окно")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setView(R.layout.dialog)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton("Отмена", null)
+                .create();
+    }
+}
