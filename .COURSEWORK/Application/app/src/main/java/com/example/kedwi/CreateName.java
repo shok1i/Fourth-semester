5dac6a8294d336fc3d121b3fd1f38afa07f369cb
shoki (main) <shilo152004@mail.ru>
2024-04-28 23:33:41 +0300

    CourseWork: Start

---
+++ .COURSEWORK/Application/app/src/main/java/com/example/kedwi/CreateName.java
@@ -0,0 +1,7 @@
+package com.example.kedwi;
+
+import androidx.fragment.app.Fragment;
+
+public class CreateName extends Fragment {
+
+}
---
+++ .COURSEWORK/Application/app/src/main/java/com/example/kedwi/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.kedwi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.kedwi.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding _binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        _binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(_binding.getRoot());
+    }
+}
